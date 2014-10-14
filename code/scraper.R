4514ec3713a236a68439199c5392242d874e90f4
Luis Capelo <luiscape@gmail.com>
2014-10-14 16:32:00 -0400

    fixing mistaken scraper

--- code/scraper.R
+++ code/scraper.R
@@ -1,67 +1,53 @@
-# Scraper for CDC's Historical Ebola outbreaks data.
+## Simple HTML scraper.
 
-# dependencies
+# Dependencies
 library(XML)
 library(RCurl)
-source('tool/code/write_tables.R')
+library(rjson)
+
+# Helper function to handle folders on SW.
+onSw <- function(there = FALSE) {
+  if (there == TRUE) f = 'tool/'
+  else f = ''
+  return(f)
+}
+
+# Helper function to write tables on ScraperWiki.
+source(paste(onSw(F), 'code/write_tables.R', sep=""))
 
 
 ###################################################
 ###################################################
-######### Scraping a List of Reports List #########
+######## Scraping Case data from HealthMap ########
 ###################################################
 ###################################################
 
-# function that gets the list of documents from WHO
-# website and assembles a nice data.frame
-scrapeCDCData <- function() {
+# Function that fetches the data available
+# in HealthMap's website and transforms
+# the results into a data table.
+scrapeHealthMap <- function() {
   cat('----------------------------------------\n')
-  cat("Collecting the table from CDC's website.\n")
+  cat("Collecting the table from HealthMap.\n")
   cat('----------------------------------------\n')
 
-  # CDC url
-  url = 'http://www.cdc.gov/vhf/ebola/outbreaks/history/chronology.html'
+  # HealthMap RSS feed
+  url = 'http://healthmap.org/rss/ebola-all.rss'
 
   # getting the html
-  doc <- htmlParse(url)
+  doc <- xmlInternalTreeParse(url)
 
   # collecting the data into a data.frame
   output <- data.frame(
-    year =  xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/th', xmlValue),
-    coutry = xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/td[1]', xmlValue),
-    ebola_subtype = xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/td[2]', xmlValue),
-    reported_number_of_human_cases = xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/td[3]', xmlValue),
-    reported_number_of_deaths_among_cases = xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/td[4]', xmlValue),
-    situation = xpathSApply(doc, '//*[@id="outbreaks"]/tbody/tr/td[5]', xmlValue)
+    title = xpathSApply(doc, '//item/title', xmlValue),
+    publication_date = xpathSApply(doc, '//item/pubDate', xmlValue),
+    source_url = xpathSApply(doc, '//item/source', xmlGetAttr, 'url'),
+    author = xpathSApply(doc, '//item/author', xmlValue),
+    country = xpathSApply(doc, '//item/category[@domain="location"]', xmlValue),
+    latitude = xpathSApply(doc, '//item/geo:lat', xmlValue),
+    longitude = xpathSApply(doc, '//item/geo:long', xmlValue),
+    description = xpathSApply(doc, '//item/description', xmlValue)
     )
 
-  # extracting the data from within parentheses in percent
-  output$reported_number_of_deaths_among_cases_percent <- regmatches(output$reported_number_of_deaths_among_cases, gregexpr("(?<=\\().*?(?=\\))", output$reported_number_of_deaths_among_cases, perl=T))
-  
-  # cleaning the parentheses out of the data and the % sign
-  output$reported_number_of_deaths_among_cases <- gsub("(?=\\().*?(?<=\\))", "", output$reported_number_of_deaths_among_cases, perl = T)
-  output$reported_number_of_deaths_among_cases_percent <- gsub('%', '', output$reported_number_of_deaths_among_cases_percent)
-  output$reported_number_of_deaths_among_cases_percent <- as.numeric(output$reported_number_of_deaths_among_cases_percent)
-  
-  # replacing 0s
-  output$reported_number_of_deaths_among_cases_percent <- ifelse(is.na(output$reported_number_of_deaths_among_cases_percent), 0, output$reported_number_of_deaths_among_cases_percent)
-  
-  # cleaning decimal cases
-  specify_decimal <- function(x, k) format(round(x, k), nsmall=k)
-  output$reported_number_of_deaths_among_cases_percent <- specify_decimal(output$reported_number_of_deaths_among_cases_percent, 0)
-  
-
-  # create ifelse for laboratory confirmed cases
-  output$laboratory_confirmed_cases <- ifelse(grepl('\\*', output$reported_number_of_deaths_among_cases), TRUE, FALSE)
-
-  # create ifelse for asymptomatic cases
-  output$asymptomatic <- ifelse(grepl('asymptomatic', output$reported_number_of_human_cases), TRUE, FALSE)
-  
-  # cleaning the parentheses out of the asymptomatic cases
-  output$reported_number_of_human_cases <- gsub("(?=\\().*?(?<=\\))", "", output$reported_number_of_human_cases, perl = T)
-  
-  # adding references
-  reference =  xpathSApply(doc, '//*[@id="contentArea"]/div[1]/div/ol/li', xmlValue)
 
   # returning results
   cat('-------------------------------\n')
@@ -71,10 +57,7 @@ scrapeCDCData <- function() {
 }
 
 # running
-cdcOutbreakList <- scrapeCDCData()
-
-# writing output in CSV
-write.csv(cdcOutbreakList, 'data/report_list.csv', row.names = F)
+healthMapData <- scrapeHealthMap()
 
 # writing table to scraperwiki
-writeTables(cdcOutbreakList, 'CDC_Outbreak_Data', 'scraperwiki')
+writeTables(healthMapData, 'health_map_data', 'scraperwiki')
